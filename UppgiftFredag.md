76c46311d1ff7c08f03dafaeb64b5dc613482f2a
Max Runesson <Max@maxRunesson.se>
2020-11-20 11:51:29 +0100

    switch to my github

---
+++ UppgiftFredag.md
@@ -0,0 +1,24 @@
+1. 
+    ROUTE                               VERB        Description             Returne
+    localhost:3000/notes                Get         Get all notes           All notes
+    localhost:3000/notes/id             Get         Get a notes             Selected note
+    localhost:3000/notes/id             Delete      Remove a note           Remove selected note
+    localhost:3000/notes                Post        Creat a new note        Create a new note
+    localhost:3000/notes/id             Put         Edite a note            Change a selected note
+
+
+
+    Variable name       Comment
+    
+    Notes               Good name
+    NotesObject         Unclear what it means
+    NotesData           Good name
+    newNOtesId          Good name
+
+    dataPath            Can be a bit unclear
+
+2.  
+    
+3.  
+    
+4.  
--- routes/notes.js
+++ routes/notes.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
-var fs = require('fs'); //filesystem
+var fs = require('fs');
+const { callbackify } = require('util');
 
 const dataPath = "./data/notes.json";
 
@@ -17,43 +18,65 @@ router.get('/', function(req, res, next) {
 
 //POST a new Note
 router.post('/', function(req, res, next) {
-
-    //jag vill läsa in hela "filen" och ta reda på hur många poster som finns
-    //fundera på JavaScript Object vs JSON som text, dvs vi har en Array med data
-    //ta antalet poster och öka med 1
-    //detta tal blir mitt nya id
-
-    var notesdata; //= JSON.parse(fs.readFile(dataPath));
+    var notesdata;
 
     fs.readFile(dataPath, (err,data) =>{
         if(err) {
             throw err;
         }
-        //console.log(JSON.parse(data));
         notesdata = JSON.parse(data);
-        //console.log(notesdata);
+
         var newNotesId = Object.keys(notesdata).length + 1;
+     
         notesdata[newNotesId] = req.body.data;
-        console.log(notesdata);
-        //fs.writeFile(dataPath, JSON.stringify(notesdata));
-    });
+        notesdata[newNotesId].id = newNotesId;
 
-    //vi FÖRVÄNTAR oss att nu är notesdata populerat med data
-    //MEN så är inte fallet!!
-    //funktionen ovan fortsätter jobba och vi hamnar hör direkt
-    //INNAN något värde har populerats till notesdata
-    console.log(notesdata); //varför funkar inte detta????
-
-    //console.log(notesdata[0].title);
-
-   //var newNotesId = Object.keys(notesdata).length + 1;
-    //notesdata[newNotesId] = req.body.data;
-
-    //fs.writeFile(dataPath, JSON.stringify(notesdata));
-
+        fs.writeFile(dataPathm, JSON.stringify(notesdata), (err) =>{
+            if(err) {
+                throw err;
+            }
+        });
+    console.log(notesdata); 
     res.status(200).send("new user added successfully");
-    
-    
-  });
+    });
+});
 
+//Get Note by id
+router.get('/:id', function(req, res, next){
+    fs.readFile(dataPath, (err,data) =>{
+        if(err){
+            throw err;
+        }
+        var id = req.params.id;
+        var notesdata = Json.parse(data);
+
+        res.send(data[id]);
+    })
+})
+
+//Delete
+router.delete('/', function(req, res, next){
+
+    fs.readFile(dataPath, (err,data) =>{
+        if(err) {
+            throw err;
+        }
+        notesdata = JSON.parse(data);
+        fs.unlinkSync(dataPath);
+
+        res.send(JSON.parse(data));
+        console.log("Data have been removed");
+    });
+});
+
+//EDITE note
+router.put('/', function(req, res, next){
+
+    fs.readFile(dataPath, (err,data) =>{
+        if(err) {
+            throw err;
+        }
+        res.send(JSON.parse(data));
+    });
+});
 module.exports = router;
