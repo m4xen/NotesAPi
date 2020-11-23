a0563ac43a5bba37c71e35bebf31a97b53dc3611
Max Runesson <Max@maxRunesson.se>
2020-11-23 11:58:36 +0100

    changes

--- routes/notes.js
+++ routes/notes.js
@@ -5,6 +5,33 @@ const { callbackify } = require('util');
 
 const dataPath = "./data/notes.json";
 
+const errorMsg = [
+	{
+		//getAll error = 0
+		"id" : 0,
+		"code": 500,
+		"description" : "Cant read From JSONFile"
+	},
+	{
+		//get Specific = 1
+		"id" : 1,
+		"code" : 400,
+		"description" : "Could not find the specified Note"
+	},
+	{
+		//post new note = 2
+		"id" : 2,
+		"code" : 400,
+		"description" : "query not defined correctly"
+	},
+	{
+		//update(put) note = 3
+		"id" : 3,
+		"code" : 400,
+		"description" : "Could not find note to edit"
+  }
+];
+
 /* GET all Notes. */
 router.get('/', function(req, res, next) {
   fs.readFile(dataPath, (err,data) =>{
@@ -42,41 +69,75 @@ router.post('/', function(req, res, next) {
 });
 
 //Get Note by id
-router.get('/:id', function(req, res, next){
-    fs.readFile(dataPath, (err,data) =>{
-        if(err){
-            throw err;
-        }
-        var id = req.params.id;
-        var notesdata = Json.parse(data);
-
-        res.send(data[id]);
-    })
-})
-
-//Delete
-router.delete('/', function(req, res, next){
-
+router.get('/:id', function(req, res, next) {
     fs.readFile(dataPath, (err,data) =>{
         if(err) {
             throw err;
         }
-        notesdata = JSON.parse(data);
-        fs.unlinkSync(dataPath);
-
-        res.send(JSON.parse(data));
-        console.log("Data have been removed");
+        var id = req.params.id;
+        var notesdata = JSON.parse(data);
+  
+        res.send(notesdata[id]);
     });
 });
 
 //EDITE note
-router.put('/', function(req, res, next){
-
+router.put('/:id', function(req, res, next) {
+    var id = req.params.id;
+    var title = req.body.title;
+    var content = req.body.content;
+    var NoteArray;
     fs.readFile(dataPath, (err,data) =>{
-        if(err) {
-            throw err;
+      if(err) {
+          console.log(err);
+          throw err;
+      }
+      //change data
+      NoteArray = JSON.parse(data);
+      NoteArray[id].title = title;
+      NoteArray[id].content = content;
+      //save to file
+      fs.writeFile(dataPath, JSON.stringify(NoteArray), (err) => { 
+        if (err) { 
+          console.log(err);
+          res.status(500).send("Could not write note[" + id + "] to file" );
         }
-        res.send(JSON.parse(data));
+        else {
+          res.status(200).send("changed note[" + id + "]");
+        }
+      }); 
     });
-});
+  });
+
+//Delete
+router.delete('/:id', function(req, res, next) {
+    var id = req.params.id;
+    var NoteArrayForDel;
+    fs.readFile(dataPath, (err,data) =>{
+      if(err) {
+          console.log(err);
+          throw err;
+      }
+      NoteArrayForDel = JSON.parse(data);
+      //NoteArrayForDel.splice(id, 0)
+      //must complete this before i can splice
+      //NoteArrayForDel.splice(id, 1);
+      //give everything a new id
+      for (var i = 0; i < NoteArray.length; i++) {
+          NoteArray[i].id = i;
+      }
+    })
+    
+    NoteArrayForDel.splice(id, 0);
+  
+    fs.writeFile(dataPath, JSON.stringify(NoteArrayForDel), (err) => { 
+      if (err) { 
+        console.log(err);
+        res.status(500).send("Could not delete[" + id + "]" );
+      }
+      else {
+        res.status(200).send("removed note[" + id + "]");
+      }
+    });
+  });
 module.exports = router;
